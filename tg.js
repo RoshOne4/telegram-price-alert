5b27bf6d5529fbce65017c1bd95eeefbb6246122
Vithush <s.vituzan@gmail.com>
2023-05-08 20:38:04 +0200

    first

---
+++ tg.js
@@ -0,0 +1,32 @@
+const { Telegraf, Markup } = require('telegraf')
+
+const token = '5949966804:AAHrBNqcQAohUBYHgCJbqaK_vOau1TsCk2s';
+const bot = new Telegraf(token)
+// const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.command('inline', (ctx) => {
+  return ctx.reply('<b>Coke</b> or <i>Pepsi?</i>', {
+    parse_mode: 'HTML',
+    ...Markup.inlineKeyboard([
+      Markup.button.callback('Coke', 'Coke'),
+      Markup.button.callback('Pepsi', 'Pepsi')
+    ])
+  })
+})
+
+bot.command('onetime', (ctx) =>
+  ctx.reply('One time keyboard', Markup
+    .keyboard(['/simple', '/inline', '/pyramid'])
+    .oneTime()
+    .resize()
+  )
+)
+
+
+bot.launch()
+
+console.log('Server running')
